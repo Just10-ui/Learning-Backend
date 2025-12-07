564717d2e7b597a61a9860c95d747aec38d9ea8f
Justin <apolongjustin49@gmail.com>
2025-12-07 15:29:50 +0800

    finished variables.js file

--- JS/variables.js
+++ JS/variables.js
@@ -12,4 +12,43 @@
   - Automatically (Not Recommended)
 */
 
-//
+// Example: using let
+let name = 'Justin'; // - The name variable contains (or stores) the value "Justin"
+
+// Example: using const
+const age = 22;
+
+// Example: using var 
+var gender = 'Male'; // !(Not Recommended)
+
+// Example: declaring automatically 
+x = 1; // !(Not Recommended)
+
+/*
+  * Variables are labels for data values.
+  * Variables are containers for storing data.
+*/
+
+/*
+  * JavaScript Identifiers
+  * Variables are identified with unique names called identifiers.
+  * Names can be short like x, y, z.
+  * Names can be descriptive age, sum, carName.
+
+  * The rules for constructing names (identifiers) are:
+
+  - Names can contain letters, digits, underscores, and dollar signs.
+  - Names must begin with a letter, a $ sign or an underscore (_).
+  - Names are case sensitive (X is different from x).
+  - Reserved words (JavaScript keywords) cannot be used as names.
+
+  ! Note
+  - Numbers are not allowed as the first character in names.
+  - This way JavaScript can easily distinguish identifiers from numbers.
+  - It's a good programming practice to declare all variables at the beginning of a script.
+*/
+
+// Example: declaring many variable with one statement
+let fName = 'Justin', 
+lName = 'Apolong', 
+mName = 'Pajares';
