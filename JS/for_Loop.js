ac0673e81ff4d02f57016c0677f71678ef95f8d4
Justin <apolongjustin49@gmail.com>
2025-12-08 16:39:28 +0800

    added a new file

---
+++ JS/for_Loop.js
@@ -0,0 +1,20 @@
+/*
+  * For Loops can execute a block of code a number of times.
+  * For Loops are fundamental for tasks like performing an action multiple times.
+*/
+
+/* 
+  * The for statement creates a loop with 3 optional expressions:
+  - for (exp 1; exp 2; exp 3) {
+    // code block to be executed
+  - }
+
+  - exp 1 is executed (one time) before the execution of the code block.
+  - exp 2 defines the condition for executing the code block.
+  - exp 3 is executed (every time) after the code block has been executed.
+*/
+
+// example of using for loop
+for (let i = 0; i < 5; i++) {
+  // code to be executed
+}
