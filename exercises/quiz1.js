4d3fdbd4d6112639a4dc2eeac6d1c010cad03089
Justin <apolongjustin49@gmail.com>
2025-12-12 13:08:48 +0800

    added a new exercise

--- exercises/quiz1.js
+++ exercises/quiz1.js
@@ -1,3 +1,5 @@
+// ? medium
+
 for (let i = 1; i <= 100; i++) {
   let output = '';
   if (i % 3 === 0) output += 'Fizz';
---
+++ exercises/quiz3.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4];
+let sum = 0;
+
+arr.forEach(items => {
+  return sum += items;
+});
+
+console.log(sum);
